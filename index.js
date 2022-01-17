d2236386af6b41a30322309d275c606dd13536e2
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-01-18 00:43:15 +0200

    got it to work!

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ document.querySelector(".img1").setAttribute("src", (imgRandom(img)));
 //and for the right image (right img element)
 document.querySelector(".img2").setAttribute("src", (imgRandom(img)));
 
-var output1 = document.querySelector(".img2").getAttribute("src", (imgRandom(img)));
-var output2 = document.querySelector(".img1").getAttribute("src", (imgRandom(img)));
+var output1 = document.querySelector(".img1").getAttribute("src", (imgRandom(img)));
+var output2 = document.querySelector(".img2").getAttribute("src", (imgRandom(img)));
 
 //change the title to display the winner
-function returnWinner () {
-    if (output1 > output2) {
-
-        var twoWins = document.querySelector("h1").innerHTML = "Player 2 Wins!";
-        var oneWins = document.querySelector("h1").innerHTML = "Player 1 Wins!";
-        var nobodyWins = document.querySelector("h1").innerHTML = "It's a draw!";
-        return twoWins;
-    } else if (document.querySelector(".img2").setAttribute("src", (imgRandom(img))) < document.querySelector(".img1").setAttribute("src", (imgRandom(img)))) {
-        return oneWins;
+function returnWinner() {
+    if (img.indexOf(output1) > img.indexOf(output2))  {
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    } else if (img.indexOf(output1) < img.indexOf(output2)) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";;
     } else {
-        return nobodyWins;
+        document.querySelector("h1").innerHTML = "It's a draw!";;
     }
 }
+
+//call the function if you want it to work :)
+returnWinner();
+
 
 
 
